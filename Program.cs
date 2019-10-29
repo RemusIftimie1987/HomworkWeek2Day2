ef6f19e517077d9204dbca4f95b723b998fb478b
Remus Iftimie <RIftimie@spartaglobal.com>
2019-10-29 16:02:20 +0000

    The 2nd homeowork.Using polymorphism

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeworkDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
